e4b0d518f58139a3467625dee3fb8730b45b87ce
Lakshya S <laks2511004@gmail.com>
2026-04-11 20:58:21 +0530

    Delete day4 directory

--- day4/launch.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo 🚀 Initializing CrewAI Blog Team UI...
-echo ---
-py -m pip install -r requirements.txt
-echo ---
-echo ✨ Installation complete. Launching the Web Dashboard...
-py -m streamlit run ui.py
-pause
